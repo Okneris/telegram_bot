e5a9dabe744dfa54f14aa20f72a49a379dd3f7b1
sirenkovv <vsirenko71@gmail.com>
2022-04-10 16:26:49 +0300

    created command start

---
+++ main.py
@@ -0,0 +1,19 @@
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    user_info = {
+        "first_name": message.from_user.first_name,
+        "last_name": message.from_user.last_name,
+    }
+    getting_message = (
+        f"Hello, {user_info.get('first_name')} {user_info.get('last_name')}!"
+    )
+    bot.send_message(message.chat.id, getting_message)
+
+
+bot.polling(none_stop=True)
